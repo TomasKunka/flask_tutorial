1e697a47beced7cf7fa3b8df8bcfb05af51c4bf8
tomas_eid <tomas.kunka@gmail.com>
2016-11-06 11:29:11 +0100

    added DB functionality

---
+++ microblog/db_upgrade.py
@@ -0,0 +1,7 @@
+#!/opt/local/Miniconda/envs/datahack/bin/python
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+v = api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+print('Current database version: ' + str(v))
